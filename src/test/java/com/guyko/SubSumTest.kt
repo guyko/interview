ce7021a80c90f846472f7cd21df823aab0877dc4
Guy Kobrinsky <gkobrinsky@outbrain.com>
2019-05-08 10:16:00 +0300

    w maxsubsum

---
+++ src/test/java/com/guyko/SubSumTest.kt
@@ -0,0 +1,22 @@
+package com.guyko
+
+import junit.framework.TestCase
+
+class SubSumTest : TestCase() {
+
+
+    fun testIt() {
+        assertEquals(16, maxsubsum(arrayOf(1, -2, 3, 4, 5, -2, 6).toIntArray()))
+    }
+
+
+    private fun maxsubsum(arr: IntArray): Int {
+        var maxSoFar = arr[0]
+        var maxEndingHere = arr[0]
+        for (i in 1 until arr.size) {
+            maxEndingHere = Math.max(maxEndingHere + arr[i], arr[i])
+            maxSoFar = Math.max(maxEndingHere, maxSoFar)
+        }
+        return maxSoFar
+    }
+}
