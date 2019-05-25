4d38903b93e76dba6bcdbb171b74d7935f7a5c33
Guy Kobrinsky <gkobrinsky@outbrain.com>
2019-05-25 17:41:52 +0300

    better list replace

--- src/test/java/com/guyko/TestLinked.kt
+++ src/test/java/com/guyko/TestLinked.kt
@@ -1,7 +1,6 @@
 package com.guyko
 
 import junit.framework.TestCase
-import java.util.*
 
 class TestLinked : TestCase() {
 
@@ -9,56 +8,40 @@ class TestLinked : TestCase() {
     fun testReverse() {
         val list = LinkedNode(1, LinkedNode(2, LinkedNode(3, LinkedNode(4, LinkedNode(5)))))
         println(list)
-        println(reverse(list, 12))
+        println(reverse(list, list, 0, 2))
+        //println(reverse(list, 12))
     }
 
 
-    private fun reverse(node: LinkedNode, k: Int): LinkedNode {
-        return reverse(node, k, 0, arrayOfNulls(k))!!
-    }
-
-    private fun reverse(node: LinkedNode?, k: Int, i: Int, buffer: Array<LinkedNode?>): LinkedNode? {
+    private fun reverse(parent: LinkedNode?,
+                        node: LinkedNode?): LinkedNode? {
         if (node == null) {
-            return buffer[0]
+            return parent
         }
         val next = node.next
-
-        buffer[i] = node
-        if (i + 1 == k) {
-            buffer[0]!!.next = reverse(next, k, 0, arrayOfNulls(k))
-
-            (0 until k - 1).forEach {
-                val i = k - it - 1
-                val item = buffer[i - 1]
-                val item2 = buffer[i]
-                if (item2 != null) {
-                    item2.next = item
-                }
-            }
-            return buffer[k - 1]
-        }
-
-        return reverse(next, k, i + 1, buffer)
-
-    }
-
-    private fun reverse(node: LinkedNode): LinkedNode {
-
-        var prev: LinkedNode? = null
-        var current: LinkedNode? = node
-        var next: LinkedNode?
-        while (current != null) {
-            next = current.next
-
-            current.next = prev
-
-            prev = current
-            current = next
-        }
-        return prev!!
-
+        node.next = parent
+        return reverse(node, next)
     }
 
 
-    private data class LinkedNode(val n: Int, var next: LinkedNode? = null)
+    private fun reverse(head: LinkedNode?,
+                        node: LinkedNode?,
+                        i: Int,
+                        k: Int): LinkedNode? {
+        if (node == null || head == null) {
+            return head
+        }
+        val next = node.next
+        return if (i + 1 == k) {
+            node.next = null
+            val sub = reverse(null, head)
+            head.next = reverse(next, next, 0, k)
+            sub
+        } else {
+            reverse(head, next, i + 1, k)
+        }
+    }
+
+    private data class LinkedNode(val n: Int,
+                                  var next: LinkedNode? = null)
 }
